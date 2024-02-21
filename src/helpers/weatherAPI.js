155e7ec83690e87f67e5f5f1522c363ef0d14bdd
JyojiTenguam <j.y.oji@hotmail.com>
2024-02-20 22:52:47 -0300

    Resolvendo requisito 01 do Projeto iChoveu

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,20 @@
 // Remova os comentários a medida que for implementando as funções
+const API_TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  try {
+    const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${API_TOKEN}&q=${term}`);
+    const data = await response.json();
+
+    if (data.length === 0) {
+      alert('Nenhuma cidade encontrada');
+    }
+
+    return data;
+  } catch (error) {
+    console.error('Ocorreu um erro ao buscar cidades:', error);
+    throw error;
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
